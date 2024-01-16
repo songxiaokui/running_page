8c3fa8e11284099de358ff823b4e91f062e52f92
austsxk <austsxk@163.com>
2024-01-16 16:40:16 +0800

    更新静态文件

--- src/static/site-metadata.ts
+++ src/static/site-metadata.ts
@@ -11,17 +11,21 @@ interface ISiteMetadataResult {
 
 const data: ISiteMetadataResult = {
   siteTitle: 'Running Page',
-  siteUrl: 'https://yihong.run',
+  siteUrl: 'https://running.austsxk.com',
   logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
-  description: 'Personal site and blog',
+  description: '武安君的日常',
   navLinks: [
     {
-      name: 'Blog',
-      url: 'https://github.com/yihong0618/gitblog',
+      name: 'AI',
+      url: 'https://nextai.sunls.de/',
     },
     {
-      name: 'About',
-      url: 'https://github.com/yihong0618/running_page/blob/master/README-CN.md',
+      name: 'AI算卦',
+      url: 'https://divination.sunls.de',
+    },
+    {
+      name: '博客',
+      url: 'https://austsxk.com',
     },
   ],
 };
